a29557ab7e3a47c6c9e469b361e55cfcac84efca
biarchitect <todd@biarchitect.com>
2014-08-13 13:55:41 -0500

    Commit Quiz 4

---
+++ Quiz 4.R
@@ -0,0 +1,48 @@
+#GetingAndCleaningData
+#Quiz 4
+#Question 1
+library(downloader)
+url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+file<-"./Data/q4q1.csv"
+download(url,mode="wb",dest=file)
+x<-read.csv(file,as.is=T, na.strings=c("..",""),skip=4)
+xnames<-names(x)
+strsplit(xnames,"wgtp")
+xnamessplit<-strsplit(xnames,"wgtp")
+xnamessplit[123]
+xnames[123]
+
+#Question 2
+url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+file<-"./Data/q4q2.csv"
+download(url,mode="wb",dest=file)
+#gdp<-read.csv(file,as.is=T, na.strings=c("..",""),skip=4,strip.white=T)
+gdp<-read.csv(file1,as.is=T,skip=4,na.strings="NA",strip.white=T)
+gdp<-gdp[gdp$X !="" & gdp$X.1 !="",]
+mean(as.numeric(gsub(",","",gdp$X.4[1:190])),na.rm=T)
+
+#Question 3
+
+grep("^United",gdp$X.3,value=T)
+
+#Question 4
+url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+file<-"./Data/q4q4b.csv"
+download(url,mode="wb",dest=file)
+#cntry<-read.csv(file,as.is=T, na.strings=c("..",""),strip.white=T)
+cntry<-read.csv(file,as.is=T)
+x<-merge(gdp,cntry,by.x="X",by.y="CountryCode")
+length(grep("*Fiscal year end: June 30*",x$Special.Notes,value=T))
+#grep("*[Jj]une*",x$Special.Notes,value=T)
+
+#Question 5
+library(quantmod)
+amzn = getSymbols("AMZN",auto.assign=FALSE)
+sampleTimes = index(amzn) 
+x2012<-sampleTimes[year(sampleTimes)==2012]
+length(x2012)
+sum(wday(x2012)==2)
+
+
+
+
